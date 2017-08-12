ac07e668bd5b0c33bbc57427c6bc14d840eb624d
Flavio <flavoleny@gmail.com>
2017-08-12 17:51:00 -0300

    Adicionando permissões com cancan

--- biblioteca/app/controllers/application_controller.rb
+++ biblioteca/app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  rescue_from CanCan::AccessDenied do |exception|
+    #redirect_to root_path,
+    redirect_to "/",
+    notice: "Você não tem permissão para acessar essa pagina"
+  end
+
 end
