de8524ef887f1f3ea3d99477d6a77d6e0663b919
Flavio <flavoleny@gmail.com>
2017-08-19 17:18:44 -0300

    Adicionando cadastro de Reservas

--- biblioteca/app/controllers/bookings_controller.rb
+++ biblioteca/app/controllers/bookings_controller.rb
@@ -1,11 +1,35 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Booking.all
+    if current_user.admin?
+      @bookings = Booking.all.paginate(page: params[:page], per_page: 2)
+    else
+      @bookings = current_user.bookings.paginate(page: params[:page], per_page: 2)
+    end
+
   end
 
   def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user_id ||= current_user.id
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :book_id, :reserved_to)
+  end
+
 end
